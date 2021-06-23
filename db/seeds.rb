2595277d54e1b1134435fdfd7758b5e748caa5fb
Rafael Costa <rafael.costta13@outlook.com>
2021-06-23 13:57:14 -0300

    create Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,122 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-	Coin.create!(ticket_coin: 'DOGEUSDT', 
+	
+	# BITCOIN
+	Coin.create!(ticket_coin: 'BTCBRL', 
+							 name: 'bitcoin',
+							 description: '',
+							 path_img: '/public/img/bitcoin.png')
+	
+	# DOGE COIN
+	Coin.create!(ticket_coin: 'DOGEBRL', 
 							 name: 'dogecoin',
 							 description: 'Bitcoin é uma criptomoeda descentralizada, sendo um dinheiro eletrônico para transações ponto-a-ponto. O primeiro artigo descrevendo uma implementação do Bitcoin foi apresentado em 2007 na lista de discussão The Cryptography Mailing por um programador ou grupo de programadores sob o pseudônimo Satoshi Nakamoto.',
 							 path_img: '/public/img/dogecoin.png')
+
+	# BNB
+	Coin.create!(ticket_coin: 'BNBBRL', 
+							 name: 'bnb',
+							 description: '',
+							 path_img: '/public/img/bnb.png')
+
+# BUSD
+	Coin.create!(ticket_coin: 'BUSDBRL', 
+							 name: 'busd',
+							 description: '',
+							 path_img: '/public/img/busd.png')
+# ETHEREUM
+	Coin.create!(ticket_coin: 'ETHBRL', 
+							 name: 'ethereum',
+							 description: '',
+							 path_img: '/public/img/ethereum.png')
+
+# TETHERUS
+	Coin.create!(ticket_coin: 'USDTBRL', 
+							 name: 'tetherUs',
+							 description: '',
+							 path_img: '/public/img/tetherus.png')
+
+# RIPPLE
+	Coin.create!(ticket_coin: 'XRPBRL', 
+							 name: 'ripple',
+							 description: '',
+							 path_img: '/public/img/ripple.png')
+	# CHANLINK
+	Coin.create!(ticket_coin: 'LINKBRL', 
+							 name: 'chain link',
+							 description: '',
+							 path_img: '/public/img/chainlink.png')
+	# LITECOIN
+	Coin.create!(ticket_coin: 'LTCBRL', 
+							 name: 'litecoin',
+							 description: '',
+							 path_img: '/public/img/litecoin.png')
+	# ADA
+	Coin.create!(ticket_coin: 'ADABRL', 
+							 name: 'ada',
+							 description: '',
+							 path_img: '/public/img/ada.png')
+# POLKADOT
+	Coin.create!(ticket_coin: 'DOTBRL', 
+							 name: 'polkadot',
+							 description: '',
+							 path_img: '/public/img/polkadot.png')
+	# CHILIZ
+	Coin.create!(ticket_coin: 'CHZBRL', 
+							 name: 'chiliz',
+							 description: '',
+							 path_img: '/public/img/chiliz.png')
+	# ENJIN COIN
+	Coin.create!(ticket_coin: 'ENJBRL', 
+							 name: 'enjin coin',
+							 description: '',
+							 path_img: '/public/img/enjincoin.png')
+	# BITTORRENT
+	Coin.create!(ticket_coin: 'BTTBRL', 
+							 name: 'bit torrent',
+							 description: '',
+							 path_img: '/public/img/bittorrent.png')
+	# WINK
+	Coin.create!(ticket_coin: 'WINBRL', 
+							 name: 'bnb',
+							 description: '',
+							 path_img: '/public/img/wink.png')
+  # HOLO
+	Coin.create!(ticket_coin: 'HOTBRL', 
+							 name: 'holo',
+							 description: '',
+							 path_img: '/public/img/holo.png')
+	# ETHEREUM CLASSIC
+	Coin.create!(ticket_coin: 'ETCBRL', 
+							 name: 'ethereum classic',
+							 description: '',
+							 path_img: '/public/img/ethereumclassic.png')
+	# POLYGON
+	Coin.create!(ticket_coin: 'MATICBRL', 
+							 name: 'polygin',
+							 description: '',
+							 path_img: '/public/img/polygon.png')
+	# SHIBA INU
+	Coin.create!(ticket_coin: 'SHIBBRL', 
+							 name: 'shiba inu',
+							 description: '',
+							 path_img: '/public/img/shibainu.png')
+	# SOLANA
+	Coin.create!(ticket_coin: 'SOLBRL', 
+							 name: 'solana',
+							 description: '',
+							 path_img: '/public/img/solana.png')
+	
+	
+	
+	
+	
+	
+	
+	
+		
+	
+	
+	
+		
